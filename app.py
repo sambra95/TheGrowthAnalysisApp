f3b5f59db9a524c63435223ab4b7fa5b4cdc533d
sambra95 <sbradley.95@outlook.com>
2026-02-10 14:27:33 +0100

    move styling code into src

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-from styling import green_gradient, green_navbar, red_buttons
+from src.styling import green_gradient, green_navbar, red_buttons
 
 st.set_page_config(
     page_title="TheGrowthAnalysisApp", layout="wide", page_icon="logo.svg"
